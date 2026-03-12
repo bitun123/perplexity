c9f21d3403df4989efa704c3cedf7a0e5d393422
satyajit <satyajitdasmahapatra2002@gmail.com>
2026-03-12 23:43:46 +0530

    create validator

---
+++ backend/src/middleware/expressValidator.js
@@ -0,0 +1,21 @@
+import {body, validationResult} from 'express-validator';
+
+
+const registerValidationRules = [
+    body('userName').matches(/^[a-zA-Z0-9]+$/).withMessage('Username must be alphanumeric').isLength({min: 3}).withMessage('Username must be at least 3 characters long'),
+    body("email").isEmail().withMessage('Invalid email address'),
+    body("password").isLength({min: 6}).matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/).withMessage('Password must contain at least one uppercase letter, one lowercase letter, and one number'),
+    (req,res,next)=>{
+        const error = validationResult(req);
+        if(!error.isEmpty()){
+            return res.status(400).json({errors: error.array()})
+        }
+        next();
+
+    }
+]
+
+
+export default {
+    registerValidationRules
+}
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,9 @@
 import {Router} from  'express';
-
+import {registerValidationRules} from '../middleware/expressValidator.js';
+import authController from '../controllers/auth.controller.js';
 
 const authRouter = Router();
 
 
 
-authRouter.post("/register")
+authRouter.post("/register", registerValidationRules, authController.registerController)
