efa6b8a4b4ed86470e28a94f16c60553fd01936e
satyajit <satyajitdasmahapatra2002@gmail.com>
2026-03-15 14:07:13 +0530

    feat: implement socket.io server initialization and connection handling

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,16 @@
 import "dotenv/config";
-import server from "./src/app.js";
+import app from "./src/app.js";
 import connectDb from "./src/config/database.js";
 
-// import { createServer } from "http";
-// import { Server } from "socket.io";
+import http from "http";
+import { initSocket } from "./src/sockets/server.socket.js";
 
-
-// const httpServer = createServer();
-// const io = new Server(httpServer, { /* options */ });
-// io.on("connection", (socket) => {
-//   socket.on("message",(msg)=>{
-//     console.log("Message received from client");
-//     console.log(msg);
-//     io.emit("sms")
-//   })
-// });
-
-
-// httpServer.listen(3000,()=>{
-//     console.log("Server is running on port 3000");
-// });
+const httpServer = http.createServer(app);
+initSocket(httpServer);
 
 
 connectDb()
 
-server.listen(3000,()=>{
+httpServer.listen(3000,()=>{
   console.log("server is running on port 3000")
 })
