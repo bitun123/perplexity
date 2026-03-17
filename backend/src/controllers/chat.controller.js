546947e248218fc5d235945f731f457985c4c62d
satyajit <satyajitdasmahapatra2002@gmail.com>
2026-03-17 17:41:59 +0530

    feat: add Mistral AI integration for chat title generation and enhance message handling

--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -1,13 +1,51 @@
-import {generateResponse} from "../services/ai.service.js";
-
+import {
+  generateGeminiResponse,
+  generateMistralChatTitle,
+} from "../services/ai.service.js";
+import chatModel from "../models/chats.model.js";
+import messageModel from "../models/message.model.js";
 
 export const sendMessage = async (req, res) => {
-    const { message } = req.body;
-    
-    const result = await generateResponse(message);
+  const { message, chat: chatId } = req.body;
 
-    res.status(200).json({
-        success: true,
-        message: result
-    })
-}
+
+  //declare title and chat variables to be used later
+  let title = null,
+    chat = null;
+//if chatId is not provided, create a new chat and generate a title for it using the first message
+  if (!chatId) {
+    title = await generateMistralChatTitle(message);
+
+    chat = await chatModel.create({
+      title,
+      user: req.user.id,
+      role: "user",
+    });
+  }
+
+  //user message should be created with the chatId if it exists, otherwise use the newly created chat's id
+  const userMessage = await messageModel.create({
+    chat: chatId || chat._id,
+    content: message,
+    role: "user",
+  });
+
+    //fetch all messages for the chat, create a new message document for the user's message, generate a response from the AI, and create a new message document for the AI's response
+  const messages = await messageModel.find({ chat: chatId || chat._id })
+
+  //generate a response from the AI using the provided message and the chat history, and create a new message document for the AI's response
+  const aiResponse = await generateGeminiResponse(messages);
+
+
+  //AI message should also be created with the chatId if it exists, otherwise use the newly created chat's id
+  const aiMessage = await messageModel.create({
+    chat: chatId || chat._id,
+    content: aiResponse,
+    role: "ai",
+  });
+
+  res.status(201).json({
+    message: "Message sent successfully",
+    AIMessage: aiMessage,
+  });
+};
--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -1,15 +1,45 @@
 import {ChatGoogleGenerativeAI} from "@langchain/google-genai"
-import {HumanMessage} from "langchain"
-const model = new ChatGoogleGenerativeAI({
+import { ChatMistralAI } from "@langchain/mistralai";
+import {AIMessage, HumanMessage,SystemMessage} from "langchain"
+const geminiModel = new ChatGoogleGenerativeAI({
     model:"gemini-2.5-flash-lite",
 apiKey:process.env.GOOGLE_GEMINI_API_KEY
 })
 
-export async function generateResponse(message) {
- const response  = await model.invoke([
-    new HumanMessage(message)
- ])
 
 
+const mistralModel = new ChatMistralAI({
+  model: "mistral-small-latest",
+    apiKey:process.env.MISTRAL_API_KEY
+})
+
+
+
+export async function generateGeminiResponse(messages) {
+ const response  = await geminiModel.invoke(
+    messages.map((msg)=>{
+        if(msg.role === "user"){
+            return new HumanMessage(msg.content)
+        }else if(msg.role === "ai"){
+            return new AIMessage(msg.content)
+        }
+    })
+ )
  return response.text;
+}
+
+
+export async function generateMistralChatTitle(message) {
+
+const response = await mistralModel.invoke([
+
+new SystemMessage(`You are a helpful assistant that answers questions concisely 
+    and descriptive till for chat conversations.
+
+    user will provide you with the first message of the chat conversation,and you will generate a concise title that captures the essence of the conversation in 2-4 words. the title should be descriptive and should give a clear idea about the topic of the conversation. The title should be in title case, meaning that the first letter of each major word should be capitalized. Do not include any special characters or punctuation in the title. The title should be relevant to the content of the conversation and should accurately reflect the main theme or subject matter being discussed.make sure the title should be 2-4 words only.
+    `),
+
+    new HumanMessage(`generate a concise title for a chat conversation with the first message: ${message}`)
+])
+return response.text;
 }
